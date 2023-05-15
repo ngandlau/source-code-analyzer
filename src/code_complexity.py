fec5f82e205933486d2a416d52ba49e07d1ff6ca
nilsgandlau <nils.gandlau@deutschebahn.com>
2023-05-15 11:36:12 +0200

    adds metric 'logical lines of code'

--- src/code_complexity.py
+++ src/code_complexity.py
@@ -1,14 +1,17 @@
 import argparse
 import ast
-from typing import Dict, List, Tuple
+from typing import List, Tuple
 from collections import namedtuple
+import astunparse
+import io
+import tokenize
 
-METRICS = ['lines_of_code']
+METRICS = ['lines_of_code', 'logical_lines_of_code']
 
 FunctionName = str
 MetricName = str
 MetricValue = int
-FunctionMetrics = namedtuple("FunctionMetrics", *METRICS)
+FunctionMetrics = namedtuple("FunctionMetrics", " ".join(METRICS))
 
 
 def get_source_code(path: str) -> str:
@@ -23,6 +26,30 @@ def count_lines_of_code(node: ast.AST) -> int:
     num_lines = end_line - start_line + 1
     return num_lines
 
+def count_docstring_lines(node: ast.AST) -> int:
+    if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef, ast.Module)):
+        if node.body and isinstance(node.body[0], ast.Expr) and isinstance(node.body[0].value, ast.Str):
+            docstring_node = node.body[0]
+            return docstring_node.end_lineno - docstring_node.lineno + 1
+    return 0
+
+def count_comment_lines(node: ast.AST) -> int:
+    function_source_code = astunparse.unparse(node)
+    comment_lines = {token.start[0] for token in tokenize.generate_tokens(io.StringIO(function_source_code).readline) if token.type == tokenize.COMMENT}
+    return len(comment_lines)
+
+def count_logical_lines_of_code(node: ast.AST) -> int:
+    """
+    Counts a functions lines of code excluding comments and docstrings.
+
+    #TODO: Currently counts an empty line as a logical line of code.
+    """
+    lines_of_code = count_lines_of_code(node)
+    lines_of_docstring = count_docstring_lines(node)
+    lines_of_comments = count_comment_lines(node)
+
+    return lines_of_code - lines_of_docstring - lines_of_comments
+
 def calculate_complexity_of_functions(path: str) -> List[Tuple[FunctionName, FunctionMetrics]]:
     source_code = get_source_code(path)
     tree = ast.parse(source_code)
@@ -34,6 +61,7 @@ def calculate_complexity_of_functions(path: str) -> List[Tuple[FunctionName, Fun
             function_name = node.name
             function_metrics = FunctionMetrics(
                 lines_of_code=count_lines_of_code(node),
+                logical_lines_of_code=count_logical_lines_of_code(node),
             )
             data.append((function_name, function_metrics))
     
